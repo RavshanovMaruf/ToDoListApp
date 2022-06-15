520ca899c03b0b991a4c9361cd43eefe29c6a87d
RavshanovMaruf <freeimmortal6@gmail.com>
2022-06-15 18:26:14 +0500

    Add project files.

---
+++ ToDoList/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ToDoList
+{
+    public class Class1
+    {
+    }
+}
---
+++ domain-entities/ToDoItemsDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace domain_entities
+{
+    public class ToDoItemsDbContext : DbContext
+    {
+        public ToDoItemsDbContext(DbContextOptions<ToDoItemsDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<ToDoItems> ToDoItem { get; set; }
+    }
+}
